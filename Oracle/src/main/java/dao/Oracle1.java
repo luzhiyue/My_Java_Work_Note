2cafac54bbacd12c0de0792a4fd91bad9cf7e81f
luzhiyue <15599020509@163.com>
2021-12-10 14:04:50 +0800

    1

---
+++ Oracle/src/main/java/dao/Oracle1.java
@@ -0,0 +1,6 @@
+package dao;
+
+public interface Oracle1 {
+    //从找出的24条数据里随机更新10条数据
+    int update();
+}
---
+++ Oracle/src/main/java/utils/DateUtils.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class DateUtils {
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
